89b4cd935ba346c7339aea86c39dae0e9c372956
Mikito Takada <mikito.takada@gmail.com>
2012-06-07 16:18:34 -0700

    Namespace support

--- test/minilog.test.js
+++ test/minilog.test.js
@@ -16,30 +16,36 @@ exports['given a minilog'] = {
     this.stream = new WriteStream();
     MiniLog.stream(this.stream);
 
-    this.log = MiniLog;
+    this.log = MiniLog();
     done();
   },
 
   'can log to stream': function(done) {
     this.log('foo');
-    assert.equal('foo', this.stream.content[0]);
+    assert.equal(this.stream.content[0], 'foo');
     done();
   },
 
   'can pass multiple arguments': function(done) {
     this.log('foo', 1, true, 'bar');
-    assert.equal('foo 1 true bar', this.stream.content[0]);
+    assert.equal(this.stream.content[0], 'foo 1 true bar');
     done();
   },
 
   'can pass object as argument and get JSON': function(done) {
     this.log('foo', { bar: 'baz' });
-    assert.equal('foo {"bar":"baz"}', this.stream.content[0]);
+    assert.equal(this.stream.content[0], 'foo {"bar":"baz"}');
     this.log([1, 2, { bar: 'baz'}]);
-    assert.equal('[1,2,{"bar":"baz"}]', this.stream.content[1]);
+    assert.equal(this.stream.content[1], '[1,2,{"bar":"baz"}]');
+    done();
+  },
+
+  'can create a namespaced logger': function(done) {
+    this.log = MiniLog('ns');
+    this.log('foo');
+    assert.equal(this.stream.content[0], 'ns foo');
     done();
   }
-
 
 };
 
