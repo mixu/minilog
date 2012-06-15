3340af44e4fafcf4510b9212ea92aceeb1d306a5
Mikito Takada <mikito.takada@gmail.com>
2012-06-14 18:08:29 -0700

    Fix localstorage bug

--- backends/browser_localstorage.js
+++ backends/browser_localstorage.js
@@ -3,8 +3,8 @@ var cache = false;
 module.exports = {
   write: function(str) {
     if(typeof window == 'undefined' || !window.localStorage ||
-       typeof JSON == 'undefined' || !JSON.stringify) return;
-    if(!cache) { cache = window.localStorage.minilog || []; }
+       typeof JSON == 'undefined' || !JSON.stringify || !JSON.parse) return;
+    if(!cache) { cache = (window.localStorage.minilog ? JSON.parse(window.localStorage.minilog) : []); }
     cache.push(new Date().toString() + ' '+ str);
     window.localStorage.minilog = JSON.stringify(cache);
   },
