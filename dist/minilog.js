c26f6ab6d429df972476b9e0bdc0d6d6c540d5eb
Mikito Takada <mikito.takada@gmail.com>
2012-07-16 17:01:31 -0700

    Typeof null reform, I wish you were here. Fix bug with serializing null.

--- dist/minilog.js
+++ dist/minilog.js
@@ -60,7 +60,7 @@ function serialize(args) {
   var items = Array.prototype.slice.call(args);
   if(!JSON || !JSON.stringify) return items;
   for(var i = 0; i < items.length; i++) {
-    if(typeof items[i] == 'object') {
+    if(items[i] && typeof items[i] == 'object') {
       // Buffers in Node.js look bad when stringified
       if(items[i].constructor && items[i].constructor.isBuffer) {
         items[i] = items[i].toString();
